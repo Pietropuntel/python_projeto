0c718e7a2fe32e864ac9ad355ddd8129e60f2e4a
Pietro Puntel <pietrosamueldossantospuntel@gmail.com>
2026-02-27 19:28:49 -0300

    commit2

--- setup/urls.py
+++ setup/urls.py
@@ -2,19 +2,15 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
-from core.views import cadastrar, inicial, entrar, primeira, principal
-
 
 urlpatterns = [
+    path('', views.inicial, name='inicial'),
     path('admin/', admin.site.urls),
-    path("inicial", inicial, name= "inicial"),
-    path("entrar", entrar, name= "entrar"),
-    path("cadastro", cadastrar, name="cadastro"),
-    path("principal", principal, name= "principal"),
-    path('reserva', views.reserva, name='reserva'),
-    path('disponibilidade', views.disponibilidade, name='disponibilidade'),
-    path('minha_reserva', views.minha_reserva, name='minha_reserva'),
-
+    path('inicial/', views.inicial, name='inicial'),
+    path('entrar/', views.entrar, name='entrar'),
+    path('cadastro/', views.cadastrar, name='cadastro'),
     path('principal/', views.principal, name='principal'),
+    path('reserva/', views.reserva, name='reserva'),
+    path('disponibilidade/', views.disponibilidade, name='disponibilidade'),
+    path('minha_reserva/', views.minha_reserva, name='minha_reserva'),
 ]
-
